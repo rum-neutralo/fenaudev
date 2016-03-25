ff4ee0fa96d8413f6451024314f8475b81f1f44f
rum-neutralo <EqA--m6WciLSnaJN!>
2016-03-25 13:05:46 +0100

    First Theme

---
+++ app/design/frontend/rum/fenau001/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magentouser
+ * Date: 25.03.16
+ * Time: 10:42
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/rum/fenau001',
+    __DIR__
+);
